cfbeda5a21473cc98fac71fe895d524de977299d
warugaki <warugaki.web.developer@gmail.com>
2021-12-28 16:54:21 +0900

    testを変更（HelloWorldコンポーネントが表示できるているか）

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello world!! read', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const readElement = screen.getByText('hello world!!');
+  expect(readElement).toBeInTheDocument();
+  expect(readElement.classList).toContain('read');
 });
